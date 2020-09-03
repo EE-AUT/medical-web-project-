0911dec97fda101d548443cc2ca0c5ed7d6fb503
ali <ali.yaghoobiann@gmail.com>
2020-09-03 16:07:21 +0430

    Home page was added

--- front/src/pages/homeForm.js
+++ front/src/pages/homeForm.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 
-class homeForm extends Component{
-    render(){
-        return(
-            <h1>hello</h1>
+class homeForm extends Component {
+    state = {
+        img: "http://zinogene.com/wp-content/uploads/2020/04/Social-Media-Video-Specs-Feature-Image.png"
+    }
+
+    imageHandler = (e) =>{
+        const reader = new FileReader();
+        reader.onload=()=>{
+            if(reader.readyState===2){
+                this.setState({img:reader.result})
+                // console.log(e.target.files[0])
+
+            }
+        }
+        reader.readAsDataURL(e.target.files[0])
+    }
+    render() {
+        const { img } = this.state
+        return (
+            <div className="custom-file">
+                <div className="img-holder">
+                    <img src={img}  alt="" className="img"></img>
+                </div>
+                    <label className="fileCustom__Input">
+                        <input type="file" id="customFile" accept="image/*" onChange={this.imageHandler}/>
+                    </label>
+
+                <input type="submit" value='Upload' className="FormField__Button mr-20" />
+
+            </div>
 
         );
     }
